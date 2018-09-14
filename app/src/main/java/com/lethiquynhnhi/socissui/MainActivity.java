ba0ec0b25d3397c0ed46b3d986356d04c13c69b9
Le Thi Quynh Nhi <ltqnhi97@gmail.com>
2018-09-14 22:42:32 +0700

    layout sociss

---
+++ app/src/main/java/com/lethiquynhnhi/socissui/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lethiquynhnhi.socissui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cau4_2);
+    }
+}
